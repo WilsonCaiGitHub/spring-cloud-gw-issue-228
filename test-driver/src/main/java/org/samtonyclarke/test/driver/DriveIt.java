53de4400c28ab0d692f05529d29fe7c53da1b53c
Tony Clarke <TONYC@amdocs.com>
2018-06-12 09:11:52 -0400

    Use netty reactor 0.7.8 and its dependencies

--- test-driver/src/main/java/org/samtonyclarke/test/driver/DriveIt.java
+++ test-driver/src/main/java/org/samtonyclarke/test/driver/DriveIt.java
@@ -13,22 +13,27 @@ public class DriveIt
 {
     public static void main(String args[]) throws InterruptedException
     {
-        Flux<Integer> intFlux = Flux.range(1, 200000);       
+        Flux<Integer> intFlux = Flux.range(1, 20000000);
         intFlux.parallel().subscribe(i -> callOnce());
+        System.out.println("Ended with no errors");
     }
 
     private static void callOnce()
     {
-        Mono<ClientResponse> exchange = WebClient.create("http://localhost:8080").get()
-                .uri("integrationTest/1.0/app-name").exchange();
-        exchange.subscribe(response ->
-        {
+        Mono<ClientResponse> exchange = WebClient.create("http://localhost:8060").get().uri("integrationTest/1.0/app-name")
+                .exchange();
+        exchange.subscribe(response -> {
             HttpStatus statusCode = response.statusCode();
             if (statusCode.is5xxServerError())
             {
                 // error has occured we can bail out
+                System.out.println("Ended with errors");
                 System.exit(-1);
             }
+        }, t -> {
+            System.out.println("Ended with error on subscribe");
+            t.printStackTrace(); 
+            System.exit(-1);
         });
     }
 
